f1c8acf16b57dc2bf09b461bb1f5c8d0fee83065
dyehya <dyehya@gmail.com>
2011-04-15 03:58:14 -0500

    Upped Voltaged to 1375mV @ 1.3GHz

--- Kernel/arch/arm/mach-s5pv210/victory/max8998_consumer.c
+++ Kernel/arch/arm/mach-s5pv210/victory/max8998_consumer.c
@@ -89,7 +89,7 @@ enum PMIC_VOLTAGE {
 static const unsigned int frequency_match_1GHZ[][4] = {
 /* frequency, Mathced VDD ARM voltage , Matched VDD INT*/
 #if 1
-        {1300000, 1350, 1125, 0},
+        {1300000, 1375, 1125, 0},
         {1200000, 1300, 1125, 0},
         {1000000, 1275, 1100, 1},
         {800000, 1200, 1100, 2},
@@ -166,7 +166,7 @@ const unsigned int (*dvs_volt_table[2])[3] = {
 };
 
 static const unsigned int dvs_arm_voltage_set[][2] = {
-	{DVSARM1, 1350},
+	{DVSARM1, 1375},
 	{DVSARM2, 1275},
 	{DVSARM3, 1100},
 	{DVSARM4, 950},
